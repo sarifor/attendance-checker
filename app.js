b686a502fc2ef758005e784fbd346770de781ea2
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-10-31 15:31:06 +0900

    Read local JSON

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-const readData = JSON.parse(data);
-console.log(readData);
+// JSON에서 Object로 바꾸기
+const jsonData = '{"location": "Starbucks", "attendance": [{"day" : "2021/11/1", "when" :  "0710", "pass" : "N", "mail" : "Y"}, {"day" : "2021/11/2", "when" :  "0700", "pass" : "Y", "mail" : "N"}, {"day" : "2021/11/3", "when" :  "0700", "pass" : "Y", "mail" : "N"}]}'; // 서버로부터 전송받은, 문자열로 전환된 객체
+const objData = JSON.parse(jsonData);
+console.log(objData);
