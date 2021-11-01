cdd72faaba04e0cf3f775aa433ded4871f504e97
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-11-01 09:48:55 +0900

    Implement static version in React, which is same with 031286a

--- src/index.js
+++ src/index.js
@@ -1,11 +1,124 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+/* 
+Div
+  Header
+  Nav
+  Main
+    List
+      ListHeader
+      ListAttendance
+        ListAttendanceDailycheckin
+    Button
+*/
+
+class ListAttendanceDailycheckin extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>{this.props.attendanceEach.day}</span>
+        <span>{this.props.attendanceEach.when}</span>
+        <span>{this.props.attendanceEach.pass}</span>
+        <span>{this.props.attendanceEach.mail}</span>                
+      </div>
+    )
+  }
+}
+
+class ListAttendance extends React.Component { // 클래스명: 대문자, Pascal Case
+  render() {
+    return ( // class가 아닌 className 사용
+      <div className="list__attendance">
+        <ListAttendanceDailycheckin attendanceEach={this.props.attendance[0]} />
+        <ListAttendanceDailycheckin attendanceEach={this.props.attendance[1]} />        
+        <ListAttendanceDailycheckin attendanceEach={this.props.attendance[2]} />
+      </div>
+    )
+  }
+}
+
+class ListHeader extends React.Component {
+  render() {
+    return (
+      <div className="list__header">
+        <h4>Day</h4>
+        <h4>When</h4>
+        <h4>Pass</h4>                                       
+        <h4>Mail</h4>
+      </div>
+    )
+  }  
+}
+
+class Button extends React.Component {
+  render() {
+    return (
+      <div className="button">
+        <button>Check In</button>
+      </div>      
+    )
+  }  
+}
+
+class List extends React.Component {
+  render() {
+    return (
+      <div className="list">
+        <ListHeader />
+        <ListAttendance attendance={this.props.attendance} />
+      </div>
+    )
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h2>Starbucks Attendance Checker</h2>      
+      </header>
+    )
+  }
+};
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <nav>
+      </nav>
+    )
+  }
+};
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <List attendance={this.props.attendance} />
+        <Button />
+      </main>
+    )
+  }
+};
+
+class Div extends React.Component { // class Div () {} (X), props ?
+  render() {
+    return (
+      <div>
+        <Header />
+        <Nav />
+        <Main attendance={this.props.attendance} />
+      </div>
+    )
+  }
+}
+
+const jsonData = '{"location": "Starbucks", "attendance": [{"day" : "2021/11/1", "when" :  "0710", "pass" : "N", "mail" : "Y"}, {"day" : "2021/11/2", "when" :  "0700", "pass" : "Y", "mail" : "N"}, {"day" : "2021/11/3", "when" :  "0700", "pass" : "Y", "mail" : "N"}]}'; // '서버로부터 전송받은, 문자열로 전환된 객체' 가정
+const objData = JSON.parse(jsonData);
+const attendance = objData.attendance;
+
+ReactDOM.render( // 여러 컴포넌트를 root element에 올릴 수 있나?  ->  작동 안 하는 걸 보니, 못 올리나봐
+  <Div attendance={attendance} />,
+  document.getElementById("root")
 );
-
