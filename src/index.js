f786525a8aa44c671f1e0ac77e31a75a935e3d50
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-11-07 20:08:32 +0900

    Test deploying

--- src/index.js
+++ src/index.js
@@ -263,21 +263,12 @@ class Div extends React.Component { // class Div () {} (X), props ?
   }
   render() {
     return (
-      <div>
-        {/*<Header />
-        <Nav />*/}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/test" element={<Main arrived="true" />} />
-            <Route path="/" element={<Main />} />
-          </Routes>
-          {/*<div>
-              <Header />
-              <Nav />
-              <Main />
-          </div>*/}          
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/test" element={<Main arrived="true" />} />
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     )
   }
 }
