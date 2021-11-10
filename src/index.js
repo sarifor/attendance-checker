f602a5a891de9d3c9940b8b0c0f211921d3f94b6
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-11-10 14:20:18 +0900

    Test deploying

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-class Div extends React.Component { // class Div () {} (X), props ?
+class Div extends React.Component { // class Div () {} (X), props ? 
   componentDidMount() {
     console.log("Div Component Mounted!");
   }
   render() {
     return (
-      <div>
-        hii
-      </div>
+      <Routes>
+        <Route path="/">
+          <div>route!!</div>
+        </Route>
+        <Route path="/test">
+          <div>Test!!</div>
+        </Route>
+      </Routes>
     )
   }
 }
 
 ReactDOM.render( // 여러 컴포넌트를 root element에 올릴 수 있나?  ->  작동 안 하는 걸 보니, 못 올리나봐
-  <Div />,
+  <BrowserRouter>
+    <Div />
+  </BrowserRouter>,
   document.getElementById("root")
 );
