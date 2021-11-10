1de11f216c038054d8b006ef7e0081829fc213b1
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-11-10 15:51:07 +0900

    Updates

--- static/js/main.afda5c7c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpattendance_checker=this.webpackJsonpattendance_checker||[]).push([[0],{17:function(e,t,c){"use strict";c.r(t);c(0);var n=c(13),r=c.n(n),s=c(14),j=c(1),a=c(6);function d(){return Object(a.jsx)(s.a,{children:Object(a.jsxs)(j.c,{children:[Object(a.jsx)(j.a,{path:"/test",children:Object(a.jsx)("div",{children:"Test"})}),Object(a.jsx)(j.a,{path:"/",children:Object(a.jsx)("div",{children:"root 1"})})]})})}r.a.render(Object(a.jsx)(d,{}),document.getElementById("root"))}},[[17,1,2]]]);
-//# sourceMappingURL=main.afda5c7c.chunk.js.map
