c61ab0c7dd1ddbe52e6922e5c20c80118b286c91
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-11-10 15:44:59 +0900

    Test deploying

--- src/index.js
+++ src/index.js
@@ -2,24 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-class Div extends React.Component { // class Div () {} (X), props ? 
-  componentDidMount() {
-    console.log("Div Component Mounted! 211110_14");
-  }
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route path="test">
-            <div>Test2</div>
-          </Route>          
-          <Route path="/">
-            <div>Test1</div>
-          </Route> 
-        </Switch>
-      </Router>
-    )
-  }
+function Div() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/test">
+          <div>Test</div>
+        </Route>          
+        <Route path="/">
+          <div>root 1</div>
+        </Route> 
+      </Switch>
+    </Router>
+  )
 }
 
 ReactDOM.render( // 여러 컴포넌트를 root element에 올릴 수 있나?  ->  작동 안 하는 걸 보니, 못 올리나봐
