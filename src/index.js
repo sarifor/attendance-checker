dd8d59169088a0f513d812643f2f08f453621386
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-11-11 19:02:52 +0900

    Test deploying

--- src/index.js
+++ src/index.js
@@ -2,20 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function Div() {
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route path="/test">
-          <div>Test</div>
-        </Route>          
-        <Route path="/">
-          <div>root 7</div>
-        </Route> 
-      </Switch>
-    </Router>
-  )
-}
+class Test extends React.Component {
+  render () {
+    return (
+      <div>Test!</div>
+    )
+  };
+};
+
+class Root extends React.Component {
+  render () {
+    return (
+      <div>Root!</div>
+    )
+  };
+};
+
+class Div extends React.Component {
+  componentDidMount() {
+    console.log("Test 1");
+  };
+
+  render () {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/test" component={Test} />
+          <Route path="/" component={Root} />
+        </Switch>
+      </Router>
+    )
+  };
+};
 
 ReactDOM.render( // 여러 컴포넌트를 root element에 올릴 수 있나?  ->  작동 안 하는 걸 보니, 못 올리나봐
   <React.StrictMode>
